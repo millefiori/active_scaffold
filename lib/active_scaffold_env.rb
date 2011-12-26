fba5b256291c4b5951959bdadd09916e77a0b8eb
Yoshimoto <toshi@millefiori.co.jp>
2011-12-26 19:51:12 +0900

    initializing on load active_controller

--- lib/active_scaffold_env.rb
+++ lib/active_scaffold_env.rb
@@ -1,12 +1,16 @@
 # TODO: clean up extensions. some could be organized for autoloading, and others could be removed entirely.
-Dir["#{File.dirname __FILE__}/active_scaffold/extensions/*.rb"].each { |file| require file }
+ActiveSupport.on_load(:action_controller) do
+  Dir["#{File.dirname __FILE__}/active_scaffold/extensions/*.rb"].each { |file| require file }
 
-ActionController::Base.send(:include, ActiveScaffold)
-ActionController::Base.send(:include, RespondsToParent)
-ActionController::Base.send(:include, ActiveScaffold::Helpers::ControllerHelpers)
+  ActionController::Base.send(:include, ActiveScaffold)
+  ActionController::Base.send(:include, RespondsToParent)
+  ActionController::Base.send(:include, ActiveScaffold::Helpers::ControllerHelpers)
+end
 ActionView::Base.send(:include, ActiveScaffold::Helpers::ViewHelpers)
 
-ActionController::Base.class_eval {include ActiveRecordPermissions::ModelUserAccess::Controller}
+ActiveSupport.on_load(:action_controller) do
+  ActionController::Base.class_eval {include ActiveRecordPermissions::ModelUserAccess::Controller}
+end
 ActiveRecord::Base.class_eval     {include ActiveRecordPermissions::ModelUserAccess::Model}
 ActiveRecord::Base.class_eval     {include ActiveRecordPermissions::Permissions}
 
